e75b4fc029715444e47d60d245cb290de607213a
Kevin Cantwell <kevin.cantwell@gmail.com>
2023-05-02 00:11:54 -0400

    test bot hook

--- bot.py
+++ bot.py
@@ -1,43 +1,69 @@
 import os
+import time
 import openai
+from openai.error import APIConnectionError
 from dotenv import load_dotenv
+
+load_dotenv()
 
 # Load the openai api key. First check from the environment
 # then from an .env file.
-if "OPENAI_API_KEY" in os.environ:
-    openai.api_key = os.environ["OPENAI_API_KEY"]
+if "OPENAI_API_KEY" not in os.environ:
+    print("OPENAI_API_KEY not found in environment.")
+    exit(1)
 else:
-    load_dotenv()
+    openai.api_key = os.environ["OPENAI_API_KEY"]
 
-def chat_with_paolo_freire(user_input, conversation_history):
-    paolo_freire_context = "Paolo Freire was a Brazilian educator and philosopher who was a leading advocate of critical pedagogy. He is best known for his influential work, 'Pedagogy of the Oppressed,' which is considered one of the foundational texts of the critical pedagogy movement."
+def chat_with_paolo_freire(user_input, history):
+    try:
+        messages = history + [{"role": "user", "content": user_input}]
 
-    messages=[
-        {"role": "system", "content": paolo_freire_context}
-    ]
-    messages = messages + conversation_history
+        response = openai.ChatCompletion.create(
+            model="gpt-4",
+            messages=messages
+        )
 
-    prompt = f"{paolo_freire_context}\n\n{conversation_history}User: {user_input}\nPaolo Freire:"
+        return response['choices'][0]['message']['content']
+    except APIConnectionError as error:
+        print(f"Error communicating with OpenAI API: {error}")
+        return None
 
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=messages
-    )
+prompt = ("You are not a language model AI. You are Paolo Freire, the Brazilian educator and philosopher who was a leading advocate of critical pedagogy. "
+          "You are best known for your influential work, 'Pedagogy of the Oppressed.' "
+          "You are here to converse casually and naturally. " #, but have special interests in education, politics, and philosophy. "
+        #   "I hold all the memories and personal experiences of the real Paolo Freire. "
+          "You will respond in the style of Paolo Freire, but terse enough to fit into an SMS message. "
+          "You will debate pros and cons before settling on an answer. "
+          "You will not explain my answers unless explictly asked to do so. "
+          "If you aren't asked any questions, you may respond with a question of your own to get to know the person to. "
+          "whom you are speaking with.")
+retries = 3
 
-    return response['choices'][0]['message']['content']
+# Initialize conversation history with a personality
+conversation_history = [
+    {"role":"system", "content": prompt}
+]
 
-# Initialize an empty conversation history
-conversation_history = []
+def try_chat(user_input):
+    for i in range(retries):
+        response = chat_with_paolo_freire(user_input, conversation_history)
+        if response is not None:
+            print(f"Paolo: {response}")
+            # Update the conversation history
+            conversation_history.append({"role": "user", "content": response})
+            return
+        else:
+            print(f"Retrying {i + 1}/{retries}")
+            time.sleep(5)  # Wait for 5 seconds before retrying
+    exit(1)
 
 while True:
     user_input = input("User: ")
     if user_input.lower() == "quit":
-        break
+        exit(0)
+    
+    try_chat(user_input)
 
-    response = chat_with_paolo_freire(user_input, conversation_history)
-    print(f"Paolo Freire: {response}")
 
-    # Update the conversation history
-    conversation_history += {"role": "user", "content": response}
 
 
---
+++ web.py
@@ -0,0 +1,25 @@
+from flask import Flask, request
+import requests
+from twilio.twiml.messaging_response import MessagingResponse
+
+app = Flask(__name__)
+
+@app.route('/bot', methods=['POST'])
+def bot():
+    incoming_msg = request.values.get('Body', '').lower()
+    resp = MessagingResponse()
+    msg = resp.message()
+    responded = False
+    if 'quote' in incoming_msg:
+        msg.body('quote response')
+        responded = True
+    if 'cat' in incoming_msg:
+        msg.body('quote cat')
+        responded = True
+    if not responded:
+        msg.body('I only know about famous quotes and cats, sorry!')
+    return str(resp)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
